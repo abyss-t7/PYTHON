277eaf808c344ec200bb7b7de549c2cef212c6bd
asimayazmrd0987-glitch <asimayazmrd0987@gmail.com>
2026-07-19 18:28:28 +0500

    Python books Pdf

--- File I-O/1.py
+++ File I-O/1.py
@@ -1,5 +1,9 @@
-f = open("/mnt/cfc2943f-24ee-4835-9138-678eac423d3c/5 Goal_Dream_Aim/PYTHON/File I-O/hiest.txt", "r")
-gui = f.read()
+f = open("/mnt/cfc2943f-24ee-4835-9138-678eac423d3c/5 Goal_Dream_Aim/PYTHON/File I-O/heist.txt", "r")
+gui = f.readline()
 print(gui)
+
+gui2= f.readline()
+print(gui2)
 f.close()
 # /mnt/cfc2943f-24ee-4835-9138-678eac423d3c/5 Goal_Dream_Aim/PYTHON/File I-O/1.py --> this is fedora
+
